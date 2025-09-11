791f93ca2423b705895d6bf16e13aae0458f6a99
ADMIN <ADMIN@USER>
2025-09-11 14:00:38 +0800

    Add project files.

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using ERC_Asset_Management_System.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERC_Asset_Management_System.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        private readonly ILogger<DashboardController> _logger;
+
+        public DashboardController(ILogger<DashboardController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Dashboard()
+        {
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
